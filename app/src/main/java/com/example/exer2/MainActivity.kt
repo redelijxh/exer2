1bedab808d49425c33adbea96de6fa1dbb944499
redelijxh <jemunoz@up.edu.ph>
2020-02-09 18:49:09 +0800

    Exer2 Muñoz

--- app/src/main/java/com/example/exer2/MainActivity.kt
+++ app/src/main/java/com/example/exer2/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var panelImage: ImageView
+    lateinit var descriptionPanel: TextView
+    lateinit var headerText: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         panelImage = findViewById(R.id.championRoller)
+        descriptionPanel = findViewById(R.id.descrip)
+        headerText = findViewById(R.id.title)
 
         val startbtn: Button= findViewById(R.id.startButton)
         startbtn.setOnClickListener{ randomChampion() }
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun randomChampion() {
+
+        headerText.setText(R.string.header_rolled)
 
         val randomInt = (1..10).random()
 
@@ -44,12 +51,29 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.vayne
         }
 
+        val stringResource = when(randomInt){
+            1-> R.string.descrip_aphelios
+            2-> R.string.descrip_caitlyn
+            3-> R.string.descrip_darius
+            4-> R.string.descrip_ekko
+            5-> R.string.descrip_kayle
+            6-> R.string.descrip_morgana
+            7-> R.string.descrip_senna
+            8-> R.string.descrip_sett
+            9-> R.string.descrip_soraka
+
+            else -> R.string.descrip_vayne
+        }
+
         panelImage.setImageResource(drawableResource)
+        descriptionPanel.setText(stringResource)
 
     }
 
     private fun resetBack(){
+        headerText.setText(R.string.header)
         panelImage.setImageResource(R.drawable.logo)
+        descriptionPanel.setText(R.string.default_descrip)
     }
 
 }
